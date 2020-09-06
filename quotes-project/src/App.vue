607ae9559a659be5cc935d127665d66498c84bc3
fsdevon <fullstackdevovernight@gmail.com>
2020-09-07 00:22:19 +0700

    Initial commit.

---
+++ quotes-project/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app" class="container">
+    <Header :quoteCount="quotes.length" :maxQuotes="maxQuotes" />
+    <NewQuote @quoteAdded="newQuote" />
+    <QuoteGrid :quotes="quotes" @quoteDeleted="deleteQuote" />
+    <div class="row mt-3">
+      <div class="col-sm-12 text-center">
+        <div class="alert alert-info">Info: Click on a Quote to delete it!</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import QuoteGrid from "./components/QuoteGrid";
+import NewQuote from "./components/NewQuote";
+import Header from "./components/Header";
+
+export default {
+  name: "App",
+  data: function () {
+    return {
+      quotes: ["Just a Quote to see something"],
+      maxQuotes: 10,
+    };
+  },
+  methods: {
+    newQuote(quote) {
+      if (this.quotes.length >= this.maxQuotes) {
+        return alert("Please delete Quotes first!");
+      }
+      this.quotes.push(quote);
+    },
+    deleteQuote(index) {
+      this.quotes.splice(index, 1);
+    },
+  },
+  components: {
+    QuoteGrid,
+    NewQuote,
+    Header,
+  },
+};
+</script>
+
+<style>
+/* #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+</style>
---
+++ quotes-project/src/components/NewQuote.vue
@@ -0,0 +1,32 @@
+<template>
+  <form>
+    <div class="row">
+      <div class="col-md-6 offset-md-3 form-group">
+        <label>Quote</label>
+        <textarea class="form-control" rows="3" v-model="quote"></textarea>
+      </div>
+      <div class="col-md-6 offset-md-3 form-group">
+        <button class="btn btn-primary" @click.prevent="createNew">Add Quote</button>
+      </div>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      quote: "",
+    };
+  },
+  methods: {
+    createNew() {
+      this.$emit("quoteAdded", this.quote);
+      this.quote = "";
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
